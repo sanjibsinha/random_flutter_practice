e953460dd82c87fdb06eab70f9f2b1e043c8bb71
sanjib <sanjib12sinha@gmail.com>
2021-01-01 13:22:03 +0530

    new feature added

---
+++ lib/view/child_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChildWidget extends StatefulWidget {
+  @override
+  _ChildWidgetState createState() => _ChildWidgetState();
+}
+
+class _ChildWidgetState extends State<ChildWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
