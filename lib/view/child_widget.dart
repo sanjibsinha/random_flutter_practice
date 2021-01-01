3b3aa5a14e88cff3c0eaca6551c2aa7fdaeb02c5
sanjib <sanjib12sinha@gmail.com>
2021-01-01 10:14:42 +0530

    adding child widget

---
+++ lib/view/child_widget.dart
@@ -0,0 +1,11 @@
+class ChildWidget extends StatefulWidget {
+  @override
+  _ChildWidgetState createState() => _ChildWidgetState();
+}
+
+class _ChildWidgetState extends State<ChildWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
